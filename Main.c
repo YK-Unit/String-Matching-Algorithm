da065c44b3e3f1ec4e3d413cd8bf91ea85ac00aa
York <yorkzhang520@gmail.com>
2014-06-25 15:26:44 +0800

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "StringMatching.h"
+
+int main(void) {
+    char *src = "0123456789a1adadc3dad3e4efg";
+    char *target = "adc3";
+    
+    int index = string_matching(src,target);
+    
+    if(index != -1){
+       size_t length_target = strlen(target);
+       printf("the src string contains the target string at index:%d",index);
+       printf("\nthe target string is: ");
+       int i = 0;
+       for(i=0; i < length_target ;i++ ){
+	      printf("%c",src[index+i]);     
+       }      
+    }else{
+       printf("the src string doesn't contain the target string...");   
+    }
+
+    getchar();
+    return 0;
+}

ee6e9cbc5f453f1c59c86fd04e2fd3ae3fed86cd
York <yorkzhang520@gmail.com>
2014-06-25 16:06:27 +0800

    Update StringMatching.h

--- StringMatching.h
+++ StringMatching.h
@@ -5,43 +5,44 @@
 #include <string.h>
 
 int string_matching(char *src,char *target){
-     size_t length_src = strlen(src);
-     size_t length_target = strlen(target);
-     
-     if(length_target > length_src){
-         return -1;             
-     }
-     
-     int point_src = 0;
-     int point_target = 0;
-     
-     while(point_src < length_src){
-          while(point_target < length_target){
-              if(src[point_src] == target[point_target]){
-                    point_target++; 
-                    point_src++;                       
-              }else{
-			  		int point_temp = point_src - point_target+1;
-			  		for(point_temp ; point_temp <= point_src; point_temp++){
-		   	             if(src[point_temp] == target[0]){
-							 point_src = point_temp;  
-							 point_src--;
-							 break;                      
-                    	 }		   				   
-				   }
-  				    
-                    break;    
-              }
+ size_t length_src = strlen(src);
+ size_t length_target = strlen(target);
+ 
+ if(length_target > length_src){
+     return -1;             
+ }
+ 
+ int point_src = 0;
+ int point_target = 0;
+ 
+ while(point_src < length_src){
+ 
+      while(point_target < length_target){
+          if(src[point_src] == target[point_target]){
+                point_target++; 
+                point_src++;                       
+          }else{
+		  		int point_temp = point_src - point_target+1;
+		  		for(point_temp ; point_temp <= point_src; point_temp++){
+	   	             if(src[point_temp] == target[0]){
+						 point_src = point_temp;  
+						 point_src--;
+						 break;                      
+                	 }		   				   
+			   }
+			    
+                break;    
           }
-          if(point_target == length_target){
-               int index = point_src - length_target;
-               return index;
-          }
-          point_target = 0;       
-          point_src++;        
-     }
-     
-     return -1;
+      }
+      if(point_target == length_target){
+           int index = point_src - length_target;
+           return index;
+      }
+      point_target = 0;       
+      point_src++;        
+ }
+ 
+ return -1;
 }
 
 #endif
